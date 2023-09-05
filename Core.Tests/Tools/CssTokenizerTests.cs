8e697c26f0355476b08794bca67959bae472fd29
Alexander Kozachenko <119358312+Alex-Kozachenko@users.noreply.github.com>
2023-09-05 16:49:33 +0300

    Simplified CssTokenizer

--- Core.Tests/Tools/CssTokenizerTests.cs
+++ Core.Tests/Tools/CssTokenizerTests.cs
@@ -9,15 +9,18 @@ public class CssTokenizerTests
     public void TokenizeCss_ShouldWork()
     {
         // TODO: name this test. I'm just outa ideas rn.
-        var sample = "main div#foo>p.bar";
-        var expected = new CssToken[]
-        {
-            new CssToken("main"),
-            new ChildCssToken("div#foo", CssDescendanceKind.Deep),
-            new ChildCssToken("p.bar", CssDescendanceKind.Child)
-        };
+        var sample = "main div>p";
+        (char?, string)[] expected = 
+        [
+            new (null, "main"),
+            new (' ', "div"),
+            new ('>', "p"),
+        ];
 
-        var result = CssTokenizer.TokenizeCss(sample);
+        var result = CssTokenizer.TokenizeCss(sample)
+            .Select(x => (x.ChildSelector, x.Css))
+            .ToArray();
+            
         Assert.That(result, Is.EquivalentTo(expected));
     }
 }
--- Core/Tools/CssTokenizer.cs
+++ Core/Tools/CssTokenizer.cs
@@ -1,19 +1,12 @@
 namespace Core.Tools;
 
-public enum CssDescendanceKind
-{
-    Deep = ' ', // main div
-    Child = '>', // main>div
-}
-
-public record class CssToken(string Css);
-public record class ChildCssToken(string Css, CssDescendanceKind kind) 
-    : CssToken(Css);
+internal record struct CssToken(char? ChildSelector, string Css);
 
 internal class CssTokenizer
 {
-    public static CssToken[]? TokenizeCss(string css)
+    internal static CssToken[] TokenizeCss(string css)
     {
+        // TODO: make an ordered enumerable.
         var result = new List<CssToken>();
         var iBase = 0;
         var separators = new[] { ' ', '>' };
@@ -22,31 +15,24 @@ internal class CssTokenizer
             if (separators.Contains(css[i]))
             {
                 var len = i - iBase;
-                result.Add(Extract(css, iBase, len));
+                result.Add(ExtractCssToken(css, iBase, len));
                 iBase = i;
             }
         }
 
-        result.Add(Extract(css, iBase, css.Length - iBase));
+        result.Add(ExtractCssToken(css, iBase, css.Length - iBase));
         return [.. result];
     }
 
-    private static CssToken Extract(string css, int iBase, int length)
+    private static CssToken ExtractCssToken(string css, int iBase, int length)
     {
-        var part = css.Substring(iBase, length);
-        return TryExtractChildToken(part) 
-            ?? new CssToken(part);
-    }
+        Span<char> childSelectors = [' ', '>'];
 
-    private static ChildCssToken? TryExtractChildToken(string part)
-    {
-        var descendanceChar = (CssDescendanceKind)part[0];
-        if (Enum.IsDefined(descendanceChar))
+        return css.Substring(iBase, length) switch
         {
-            var token = part[1..];
-            return new ChildCssToken(token, descendanceChar);
-        }
-
-        return null;
-    }
+            var token when childSelectors.Contains(token.First())
+                => new(token[0], token[1..]),
+            var token => new(null, token)
+        };
+    }        
 }
