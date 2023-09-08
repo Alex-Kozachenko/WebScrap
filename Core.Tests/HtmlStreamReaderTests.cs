9b36d7dffdf4e118ef1465e7c973332968747f17
Alexander Kozachenko <119358312+Alex-Kozachenko@users.noreply.github.com>
2023-09-08 13:31:06 +0300

    HtmlReads all tags now

--- Core.Tests/HtmlStreamReaderTests.cs
+++ Core.Tests/HtmlStreamReaderTests.cs
@@ -3,19 +3,19 @@ namespace Core.Tests;
 [TestFixture]
 public class HtmlStreamReaderTests
 {
-    [TestCase("main div.foo p")]
-    [TestCase("main div.foo>p")]
+    [TestCase("main div p")]
+    [TestCase("main div>p")]
     public void Read_ShouldReturn_InnerText(string css)
     {
         var html = """
         <main>
-            <div class="foo">
+            <div>
                 <p> One </p>
             </div>
         </main>
         """;
 
-        var expected ="main";
+        var expected ="p";
         // var expected = " One "; // final case
         var readResult = new HtmlStreamReader().Read(html, css);
         Assert.That(readResult, Is.EqualTo(expected));
--- Core/HtmlStreamReader.cs
+++ Core/HtmlStreamReader.cs
@@ -7,19 +7,24 @@ public class HtmlStreamReader
     public string Read(ReadOnlySpan<char> htmlSpan, string css)
     {
         var cssTokens = CssTokenizer.Default.TokenizeCss(css);
-        // 1. find by tag <main>
-        var beginTagIndex = htmlSpan.IndexOf('<') + 1;
-        var peekSpan = cssTokens.Peek().Css.Span;
-        var tagValueSpan = htmlSpan.Slice(
-            beginTagIndex, 
-            peekSpan.Length);
-        
-        if (tagValueSpan.SequenceEqual(peekSpan))
+
+        ReadOnlySpan<char> tagValueSpan = null;
+        while (cssTokens.Count is not 0)
         {
-            return tagValueSpan.ToString();
+            var peek = cssTokens.Peek().Css.Span;
+            var beginTagIndex = htmlSpan.IndexOf('<') + 1;
+            tagValueSpan = htmlSpan.Slice(
+                beginTagIndex, 
+                peek.Length);
+
+            if (tagValueSpan.SequenceEqual(peek) is not true)
+            {
+                return string.Empty;
+            }
+            htmlSpan = htmlSpan[(beginTagIndex+peek.Length)..];
+            cssTokens.Dequeue();
         }
-        // 2. find nested tag <p>
-        // 3. figure out the revelance
-        return string.Empty;
+        
+        return tagValueSpan.ToString();
     }
 }
