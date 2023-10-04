a4eaf011631e09508e31bdef33d1d49bf253ff3a
Alexander Kozachenko <119358312+Alex-Kozachenko@users.noreply.github.com>
2023-10-04 09:12:45 +0300

    review fix

--- Api/WebScrap.API.KnownIssues.Tests/Features/CssSelectors_KnownIssues_Tests.cs
+++ Api/WebScrap.API.KnownIssues.Tests/Features/CssSelectors_KnownIssues_Tests.cs
@@ -2,13 +2,13 @@ namespace WebScrap.API.KnownIssues.Tests;
 
 public class CssSelectors_KnownIssues_Tests
 {
-    [TestCase("*", Description="*")]
-    [TestCase("div>*", Description="*")]
-    [TestCase("*>div", Description="*")]
-    [TestCase("div *", Description="*")]
-    [TestCase("* div", Description="*")]
-    [TestCase("[data]", Description="[]")]
-    [TestCase(".some.class, .some.other.class", Description=",")]
+    [TestCase("*", Description="*", Category=Categories.KnownIssues)]
+    [TestCase("div>*", Description="*", Category=Categories.KnownIssues)]
+    [TestCase("*>div", Description="*", Category=Categories.KnownIssues)]
+    [TestCase("div *", Description="*", Category=Categories.KnownIssues)]
+    [TestCase("* div", Description="*", Category=Categories.KnownIssues)]
+    [TestCase("[data]", Description="[]", Category=Categories.KnownIssues)]
+    [TestCase(".some.class, .some.other.class", Description=",", Category=Categories.KnownIssues)]
     public void ExtractHtml_FailsToDetect_SomeCss(string css)
     {
         var html = "<div> LoremIpsum </div>";
