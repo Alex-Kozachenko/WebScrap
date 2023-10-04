bf549a49a77a5de47a1f40840e69fd116b66a554
Alexander Kozachenko <119358312+Alex-Kozachenko@users.noreply.github.com>
2023-10-04 10:20:10 +0300

    Introduced tests Refactored API

---
+++ Api/WebScrap.API.Tests/Features/DeepChildLocationTests.cs
@@ -0,0 +1,69 @@
+namespace WebScrap.API.Tests.Features;
+
+public class DeepChildLocationTests
+{
+    
+    [TestCase(Category=Categories.Features)]
+    public void Test1()
+    {
+        var css = ".container b";
+        var html = """
+        <main>
+            <div class='container'>
+                <b> Important </b>
+                <p> LoremIpsum </p>
+                <p> Lorem <b> Ipsum </b> </p>
+            </div>
+        </main>
+        """;
+
+        string[] expected = [
+            "<b> Important </b>",
+            "<b> Ipsum </b>"];
+        
+        var actual = Extract.Html(html, css);
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [TestCase(Category=Categories.Features)]
+    public void Test2()
+    {
+        var css =".container b";
+        var html = """
+        <main class="container">
+            <div class="container">
+                <p> Lorem <b> Ipsum </b> </p>
+            </div>
+            <div>
+                <p> Lorem <b> Ipsum </b> </p>
+            </div>   
+        </main>
+        """;
+
+        string[] expected = [
+            "<b> Ipsum </b>",
+            "<b> Ipsum </b>"];
+
+        var actual = Extract.Html(html, css);
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [TestCase(Category=Categories.Features)]
+    public void Test3()
+    {
+        var css =".container b";
+        var html = """
+        <main class="container">
+            <p> Lorem <b> Ipsum </b> </p>
+            <b> Clatu <b> Barata <b> Nictu </b> </b> </b>
+        </main>
+        """;
+
+        string[] expected = [
+            "<b> Ipsum </b>",
+            "<b> Clatu <b> Barata <b> Nictu </b> </b> </b>"];
+
+        var actual = Extract.Html(html, css);
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+}
