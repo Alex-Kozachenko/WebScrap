5001d5310f530f0c4319e932b0142a9163b489a9
Alexander Kozachenko <119358312+Alex-Kozachenko@users.noreply.github.com>
2023-10-17 12:21:47 +0300

    added test

--- Export/Csv/WebScrap.Export.Csv.Tests/Tools/HtmlTests.cs
+++ Export/Csv/WebScrap.Export.Csv.Tests/Tools/HtmlTests.cs
@@ -3,8 +3,15 @@ namespace WebScrap.Export.Csv.Tools.Tests;
 public class HtmlTests
 {
     [TestCase("<span>Foo</span>", ExpectedResult = "Foo")]
-    public string Strip_SingleTag_ShouldWork(string html)
+    public string Strip_SingleTag_ShouldWork(string html) 
+        => Html.Strip(html);
+
+    [Test]
+    public void Strip_SingleTag_Nested_ShouldWork()
     {
-        return Html.Strip(html);
+        var html = "<div>Foo <p>Bar <span>Buzz <span>raB </p>ooF</div>";
+        var expected = "Foo Bar Buzz raB ooF";
+        var actual = Html.Strip(html);
+        Assert.That(actual, Is.EquivalentTo(expected));
     }
 }
