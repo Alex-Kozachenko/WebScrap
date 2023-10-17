bab1b1f20829c83006c31ba8880dc31f6a9ecb7c
Alexander Kozachenko <119358312+Alex-Kozachenko@users.noreply.github.com>
2023-10-17 13:40:59 +0300

    Added json api (#36)
    * Added json api

---
+++ Modules/Export.Json/WebScrap.Modules.Export.Json/JsonApi.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using WebScrap.Modules.Export.Json.Tools;
+
+namespace WebScrap.Modules.Export.Json;
+
+public class JsonApi
+{
+    public static JsonArray Export(ReadOnlyMemory<char>[] tags)
+    {
+        var result = new List<JsonNode>();
+        foreach (var tag in tags)
+        {
+            var strippedTag = Html.Strip(tag.Span);
+            var obj = new { value = strippedTag };
+            result.Add(JsonSerializer.SerializeToNode(obj)!);
+        }
+
+        return new JsonArray([..result]);
+    }
+}
